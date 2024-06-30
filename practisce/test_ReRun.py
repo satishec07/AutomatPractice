44544ddad597f308f1201e0151f32fd610cdb0f2
satishec07 <satishec2017@gmai.com>
2024-06-30 20:22:51 +0530

    first commit

---
+++ practisce/test_ReRun.py
@@ -0,0 +1,19 @@
+def test_M1():
+    print("Method M1")
+    assert 5 == 8
+
+
+def test_M2():
+    print("Method M2")
+    assert 50 == 800
+
+
+def test_M3():
+    print("Method M3")
+    assert "50" == 69
+
+
+def test_M4():
+    print("Method M4")
+    assert "TT" == "TT"
+
